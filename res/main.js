4a5671da45bc588ca45daba1f04446ddb8e41de3
HQM <hqm03ster@gmail.com>
2015-02-13 18:08:19 +0800

    backup

--- res/main.js
+++ res/main.js
@@ -11,11 +11,12 @@ var g_layout={
 		{object_type:W.Edit,
 			id:"$test",
 			//font:UI.Font("res/fonts/cmunrm.ttf",32),
-			styles:[{font:UI.Font("res/fonts/cmunrm.ttf",32),color:0xff000000}, {font:UI.Font("res/fonts/cmunss.ttf",48),color:0xff000000}],
+			styles:[{font:UI.Font("res/fonts/cmunrm.ttf",32),color:0xff000000}, {font:UI.Font("res/fonts/cmunss.ttf",44),color:0xff000000}],
 			//styles:[{font:UI.Font("calibri",32),color:0xff000000}],
 			state_handlers:["renderer_fancy"],
 			anchor_align:"fill",anchor_valign:"fill",
 			wrap_width:1024,
+			h_blank_line:40,//todo: should be style dependent
 			text:IO.ReadAll("mo/test/example.txt"),
 			///////////////
 			show_background:1,bgcolor:0xffffffff,
@@ -30,7 +31,7 @@ UI.Application=function(id,attrs){
 		///////////////////
 		UI.Begin(W.Window('app',{
 				title:'Mini-Office',w:1280,h:720,bgcolor:0xffbbbbbb,
-				designated_screen_size:1440,flags:UI.SDL_WINDOW_MAXIMIZED|UI.SDL_WINDOW_RESIZABLE,
+				designated_screen_size:1080,flags:UI.SDL_WINDOW_MAXIMIZED|UI.SDL_WINDOW_RESIZABLE,
 				is_main_window:1}));
 			if(UI.Platform.ARCH!="mac"&&UI.Platform.ARCH!="ios"){
 				W.Hotkey("",{key:"ALT+F4",action:function(){UI.DestroyWindow(UI.top.app)}});
